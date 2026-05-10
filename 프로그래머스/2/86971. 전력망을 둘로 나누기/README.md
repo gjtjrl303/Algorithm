2d8e7abe412cc8c09884737580489d93bfe71956
HeoSeokJun <gjtjrl303@naver.com>
2026-05-10 13:42:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.22 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971#) 
 
 ### 성능 요약
 
-메모리: 81.7 MB, 시간: 10.20 ms
+메모리: 74.4 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 10일 13:40:41
+2026년 05월 10일 13:42:25
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,67 +1,30 @@
-import java.util.*;
 
 class Solution {
-    
-    boolean[][] graph;
-    int min = Integer.MAX_VALUE;
-    
+    int N, min;
+    int[][] map;
+    int[] vst;
+    int dfs(int n){
+        vst[n] = 1;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(vst[i] == 0 && map[n][i] == 1) {
+                vst[i] = 1;
+                child += dfs(i);
+            }
+        }
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;
+    }
     public int solution(int n, int[][] wires) {
-        graph = new boolean[n+1][n+1];
-     
-        
-        for(int i = 0 ; i < wires.length ; i++){
-            graph[wires[i][0]][wires[i][1]] = true;
-            graph[wires[i][1]][wires[i][0]] = true;
+        N = n;
+        min = n;
+        map = new int[n+1][n+1];
+        vst = new int[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
         }
-          
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= n ; j++){
-                boolean[] visited = new boolean[n+1];
-                if(graph[i][j]){
-                    graph[i][j] = false;
-                    graph[j][i] = false;
-                    
-                    int size = calculateSize(i, n, visited);
-                    min = Math.min(min, Math.abs(size - (n - size)));
-                    
-                    graph[i][j] = true;
-                    graph[j][i] = true;
-                }
-            }
-        }
-        
+        dfs(1);
         return min;
-    }
-    
-    public int calculateSize(int currentNode, int nodeCount , boolean[] visited){
-        
-        int size = 1;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(currentNode);
-        visited[currentNode] = true;
-        
-        while(!queue.isEmpty()){
-            int targetNode = queue.poll();
-            
-            for(int i = 1 ; i <= nodeCount ; i++){
-                if(!visited[i] && graph[targetNode][i]) {
-                    queue.offer(i);
-                    visited[i] = true;
-                    size++;
-                }
-            }
-        }
-        
-        return size;
-    }
-    
-    
-    public void printGraph(){
-         for(int i = 1 ; i < graph.length ; i++){
-            for(int j = 1 ; j < graph.length ; j++){
-                System.out.print(graph[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 }
